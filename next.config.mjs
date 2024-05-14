0910c8cbf3c58ac51f0175baa296b57229d83506
Buns Enchantress <bunsthedev@gmail.com>
2024-05-14 16:52:27 -0500

    update: contract, config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'media.giphy.com'
+        ]
+    }
+};
 
 export default nextConfig;
--- src/lib/contract.sol
+++ src/lib/contract.sol
@@ -3,16 +3,13 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
-contract AccessToken is ERC721, Ownable {
+contract OpenAccess is ERC721 {
     uint256 private _nextTokenId;
 
-    constructor(
-        address initialOwner
-    ) ERC721("Another Typical Access Token", "ATAT") Ownable(msg.sender) {}
+    constructor() ERC721("Open Access Token", "OPEN") {}
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to) public {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
