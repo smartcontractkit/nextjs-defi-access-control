7da67bca0eba4a735f4db82942778848c2ee11b9
Buns Enchantress <bunsthedev@gmail.com>
2024-05-20 13:25:19 -0500

    refactor: GrantAccessToken
    Externalized components to isolate distinct functionality from one component to another. Namely, handling Wallet Connection and Access Token access outside of the granting component.

--- src/app/components/GrantAccessButton.tsx
+++ src/app/components/GrantAccessButton.tsx
@@ -1,12 +1,9 @@
 "use client"
 
 import React, { useState } from "react"
-import Image from "next/image"
-import Link from "next/link"
 import { getContract } from "viem"
 import { sepolia } from "viem/chains"
 
-import { ConnectBlockchain } from "@/lib/BlockchainClient"
 import { ConnectWalletClient } from "@/lib/WalletClient"
 import { ACCESS_TOKEN_ADDRESS } from "../constants/addresses"
 import { ACCESS_ABI } from "../constants/abis"
@@ -14,57 +11,33 @@ import { ACCESS_ABI } from "../constants/abis"
 export default function GrantAccessButton() {
     // State variables to store the wallet address and balance
     const [walletAddress, setWalletAddress] = useState('')
-    const [balance, setBalance] = useState('0')
-
-    async function handleConnect() {
-        try {
-            // instantiates: a Wallet Client and a Public Client
-            const walletClient = await ConnectWalletClient();
-            const blockchainClient = ConnectBlockchain();
-
-            const nftContract = getContract({
-                // The contract address of the NFT contract
-                address: ACCESS_TOKEN_ADDRESS,
-                abi: ACCESS_ABI,
-                // @ts-ignore
-                client: blockchainClient,
-            });
-            // @ts-ignore
-            //   const nftOwner = await nftContract.read.owner();
-            // Retrieve the wallet address using the Wallet Client
-            const [address] = await walletClient.requestAddresses();
-            // updates: the state variables with the retrieved address and balance.
-            setWalletAddress(address);
-
-            await walletClient.switchChain({ id: sepolia.id });
-            // @ts-ignore
-            const nftBalance = await nftContract.read.balanceOf([address]);
-            console.log("nftBalance", nftBalance);
-
-            setBalance(parseInt(nftBalance)?.toString() ?? '0');
-
-        } catch (error) {
-            // Error handling: Display an alert if the transaction fails
-            alert(`Transaction Failed: ${error}`);
-        }
-    }
+    const [accessGranted, setAccessGranted] = useState(false)
 
     async function handleMint(toAddress: string) {
         try {
             console.log("toAddress", toAddress);
-            // Instantiate a Wallet Client and a Public Client
+            // instantiates: Wallet Client and a Public Client.
             const walletClient = await ConnectWalletClient();
             const nftContract = getContract({
-                // The contract address of the NFT contract
                 address: ACCESS_TOKEN_ADDRESS,
                 abi: ACCESS_ABI,
                 // @ts-ignore
                 client: walletClient,
             });
             console.log("nftContract", nftContract);
-            // Retrieve the wallet address using the Wallet Client
+            // retrieves: wallet address using the Wallet Client.
             const [address] = await walletClient.requestAddresses();
+            await setWalletAddress(address);
+            console.log('wally', walletAddress);
             await walletClient.switchChain({ id: sepolia.id });
+            // @ts-ignore
+            const nftBalance = await nftContract.read.balanceOf([address]);
+            // console.log("nftBalance", nftBalance);
+
+            setAccessGranted(
+                parseInt(nftBalance)?.toString() !== '0' ? true : false
+            );
+
             // runs: grantAccess(address to)
             const hash = await walletClient.writeContract({
                 account: address,
@@ -75,73 +48,25 @@ export default function GrantAccessButton() {
                 chain: sepolia
             });
 
-            await hash
+            await hash 
 
-        } catch (error) {
+    } catch (error) {
             // Error handling: Display an alert if the transaction fails
+            setAccessGranted(false)
             alert(`Transaction Failed: ${error}`);
         }
     }
 
     return (
-        <div>
-            <div>
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    {!walletAddress ? (
-                        <button
-                            className={`px-8 py-2 rounded-md bg-slate-400 flex flex-row items-center justify-center border border-[#1e2124] hover:border hover:border-indigo-600 shadow-md shadow-indigo-500/10`}
-                            onClick={handleConnect}
-                        >
-                            <h1
-                                className="mx-auto"
-                            >
-                                {`Connect Wallet`}
-                            </h1>
-                        </button>
-                    ) : (
-                        <div className="flex items-center">
-
-                            <div className="grid gap-4 items-center">
-                                {/* <div className="text-xs md:text-xs">
-                                    {walletAddress} <br /> Balance: {balance}
-                                </div> */}
-                                <button
-                                    onClick={() => handleMint(walletAddress)}
-                                    className="px-8 py-2 rounded bg-purple-500 text-white"
-                                >
-                                    Grant Access
-                                </button>
-                                <Link
-                                    href={"https://remix.ethereum.org/#url=https://github.com/BunsDev/nextjs-defi-access-control/blob/develop/src/lib/OpenAccess.sol&lang=en&optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.25+commit.b61c2a91.js"}
-                                    target="_blank"
-                                    className="px-8 py-1 rounded bg-blue-500 text-white"
-                                >
-                                    <button>
-                                        Open Code in Remix (Ethereum IDE)
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    )}
-                </p>
+        <button
+            onClick={() => handleMint(walletAddress)}
+            className={accessGranted ? 'hidden' : ''}
+        >
+            <div className="p-4 text-lg w-full text-center justify-center items-center flex bg-blue-700 rounded-xl text-white font-bold">
+                <div className="flex items-center">
+                    {`Grant Access`}
+                </div>
             </div>
-            <div
-                className={"flex w-full mt-24 items-center justify-center font-mono text-sm"}
-            >
-                {balance != '0' ? (
-                    <Image
-                        src={"https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDhqN3k5ZmI0dHd4ZGFxNnhlNWkzaHZxOWR5ZmFybTB5bWFkMmhreCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IgpsRT6FdinjrDWzNu/giphy.gif"}
-                        height={400}
-                        width={400}
-                        alt={'VIP token'} />
-                ) : (
-                    <Image
-                        src={"https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWUweDVkMHp1Zno0MXJvdnpmcnNycTd6bjBmdGxvc21qdGEzMDI4dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l1J9EdzfOSgfyueLm/giphy.gif"}
-                        height={400}
-                        width={400}
-                        alt={'Basic User'} />
-                )}
-            </div>
-        </div>
+        </button>
     );
 }
