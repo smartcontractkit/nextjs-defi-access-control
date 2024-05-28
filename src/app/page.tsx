66ab12cb8e30c8e9f8027b1dbd8223ab6b18d63b
Buns Enchantress <bunsthedev@gmail.com>
2024-05-28 16:41:01 -0500

    use: GrantAccessView, remove: ConnectButton
    This enables a seamless communication without having to setup state for a basic UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import ConnectButton from "./components/ConnectButton"
-import GrantAccessButton from "./components/GrantAccessButton"
+import GrantAccessView from "./components/GrantAccessView"
 import ViewCodeButton from "./components/ViewCodeButton"
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
-        <ConnectButton />
-      </div>
+
       <div
         className={"grid grid-cols-1 gap-4 w-full max-w-5xl items-center justify-between font-mono text-sm"}
       >
-        <GrantAccessButton />
+        <GrantAccessView />
         <ViewCodeButton />
       </div>
     </main>
